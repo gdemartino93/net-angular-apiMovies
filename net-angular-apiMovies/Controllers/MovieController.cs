e448bc45b145ae0348a9af3f905b3e48cdaa1568
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-16 22:23:29 +0200

    controller with crud implementation

--- net-angular-apiMovies/Controllers/MovieController.cs
+++ net-angular-apiMovies/Controllers/MovieController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using net_angular_apiMovies.Models.Domain;
+using net_angular_apiMovies.Models.DTOs;
 using net_angular_apiMovies.Repositories.Abstract;
 
 namespace net_angular_apiMovies.Controllers
@@ -30,7 +31,24 @@ namespace net_angular_apiMovies.Controllers
         [HttpPost]
         public IActionResult AddUpdate(Movie movie)
         {
-
+            var result = _movieRepository.AddUpdate(movie);
+            var status = new Status
+            {
+                StatusCode = result ? 1 : 0,
+                Message = result ? "Film aggiunto correttamente" : "C'è stato un errore"
+            };
+            return Ok(status);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var data = _movieRepository.Delete(id);
+            var status = new Status
+            {
+                StatusCode = data ? 1 : 0,
+                Message = data ? "Film eliminato" : "C'è stato un errore"
+            };
+            return Ok(data);
         }
     }
 }
