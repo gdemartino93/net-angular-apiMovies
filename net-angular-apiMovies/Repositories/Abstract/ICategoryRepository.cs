a48656ae960e1aea86c4a896e001a437b4f66ef1
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-16 22:32:13 +0200

    category with controller

--- net-angular-apiMovies/Repositories/Abstract/ICategoryRepository.cs
+++ net-angular-apiMovies/Repositories/Abstract/ICategoryRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace net_angular_apiMovies.Repositories.Abstract
+﻿using net_angular_apiMovies.Models.Domain;
+
+namespace net_angular_apiMovies.Repositories.Abstract
 {
     public interface ICategoryRepository
     {
+        bool AddUpdate(Category category);
+        bool Delete(int id);
+        Category GetById(int id);
+        IEnumerable<Category> GetAll();
     }
 }
---
+++ net-angular-apiMovies/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,67 @@
+﻿using net_angular_apiMovies.Models.Domain;
+using net_angular_apiMovies.Repositories.Abstract;
+
+namespace net_angular_apiMovies.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DatabaseContext _ctx;
+
+        public CategoryRepository(DatabaseContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public bool AddUpdate(Category category)
+        {
+            try
+            {
+                if(category.Id == 0)
+                {
+                    _ctx.Add(category);
+                }
+                else
+                {
+                    _ctx.Update(category);
+                }
+                _ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var categorySearched = _ctx.Categories.Find(id);
+                if (categorySearched == null)
+                {
+                    return false;
+                }
+                _ctx.Remove(categorySearched);
+                _ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _ctx.Categories.ToList();
+        }
+
+        public Category GetById(int id)
+        {
+            return _ctx.Categories.Find(id);
+        }
+    }
+}
