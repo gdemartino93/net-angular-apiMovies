423564c62684d15b4ba610531148b374d725b27a
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-16 22:19:23 +0200

    create status class to handle the http response

---
+++ net-angular-apiMovies/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using net_angular_apiMovies.Models.Domain;
+using net_angular_apiMovies.Repositories.Abstract;
+
+namespace net_angular_apiMovies.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MovieController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var data = _movieRepository.GetAll();
+            return Ok(data);
+        }
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var data = _movieRepository.GetById(id);
+            return Ok(data);
+        }
+        [HttpPost]
+        public IActionResult AddUpdate(Movie movie)
+        {
+
+        }
+    }
+}
---
+++ net-angular-apiMovies/Models/DTOs/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace net_angular_apiMovies.Models.DTOs
+{
+    public class Status
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+    }
+}
