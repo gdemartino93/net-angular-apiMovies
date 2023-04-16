4f04c60dd4ce2b553ac1015a7218c9fbebec94ed
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-16 20:34:49 +0200

    create modeol category

---
+++ net-angular-apiMovies/Models/Domain/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_angular_apiMovies.Models.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
